05f76a4121412798f3c6afb23482fe33f27b68e7
meijie555 <1582384970@qq.com>
2020-03-10 17:59:19 +0800

    首页布局完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,24 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
-import Login from '@/views/Login/index.vue'
+import Login from '@/views/Login'
+import Home from '@/views/Home'
+import Welcome from '@/views/Welcome'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { path: '/login', component: Login }
+    // 登录
+    { path: '/login', component: Login },
+    // 首页
+    {
+      path: '/',
+      component: Home,
+      children: [
+        // 欢迎
+        { path: '/', component: Welcome }
+      ]
+    }
   ]
 })
 
