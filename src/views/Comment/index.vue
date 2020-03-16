d131617191b31a3c96081e851f93ab29a0069686
meijie555 <1582384970@qq.com>
2020-03-16 15:35:15 +0800

    评论管理组件完成

--- src/views/Comment/index.vue
+++ src/views/Comment/index.vue
@@ -14,8 +14,8 @@
         </el-table-column>
         <el-table-column label="操作" width="120">
           <template slot-scope="scope">
-            <el-button v-if="scope.row.comment_status" type="danger" size="small">关闭评论</el-button>
-            <el-button v-else type="success" size="small">打开评论</el-button>
+            <el-button @click="toggleStatus(scope.row)" v-if="scope.row.comment_status" type="danger" size="small">关闭评论</el-button>
+            <el-button @click="toggleStatus(scope.row)" v-else type="success" size="small">打开评论</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -53,6 +53,26 @@ export default {
     this.getArticles()
   },
   methods: {
+    // 切换 评论状态  正常  关闭
+    toggleStatus (row) {
+      // row 有 id  comment_status
+      const text = row.comment_status ? '您确认关闭该文章的评论功能吗？如果关闭用户将不能对该文章进行评论' : '您确认打开评论功能吗？'
+      // 打开对话框
+      this.$confirm(text, '温馨提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        // 发修改状态的请求
+        const { data: { data } } = await this.$http.put(`comments/status?article_id=${row.id}`, {
+          allow_comment: !row.comment_status
+        })
+        // 成功
+        this.$message.success(data.allow_comment ? '打开评论成功' : '关闭评论成功')
+        // 更新当前文章的状态
+        row.comment_status = data.allow_comment
+      }).catch(() => {})
+    },
     // 分页函数
     pager (newPage) {
     // 修改当前的页码为新的页码
